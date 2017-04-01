c51ebce696b20bfd33ea975f076da602f5d3e96e
YuanSyun <rty3497374@gmail.com>
2017-04-01 11:28:41 +0800

    Add files via upload

---
+++ Header.h
@@ -0,0 +1,10 @@
+//Created by 葉元勳 in 2016/03/30.
+
+#include<iostream>
+//使用clock()
+#include<ctime>
+#include<cmath>
+using namespace std;
+
+//宣告
+void add(int&, int&);
---
+++ Main.cpp
@@ -0,0 +1,42 @@
+#include "Header.h"
+
+int main(){
+	int iStartTime, iEndTime;
+	int iNumber_1 = 10, iNumber_2 = 100;
+
+	cout << "ㄏノCall-by-reference\n";
+
+	cout << "肚function把计\n";
+	cout << "把计1\n";
+	cout << "\tvalue = " << iNumber_1 << endl;
+	cout << "\treference = iNumber_1" << endl;
+	cout << "\tpointer = " << &iNumber_1 << endl;
+	cout << "把计2\n";
+	cout << "\tvalue = " << iNumber_2 << endl;
+	cout << "\treference = iNumber_2" << endl;
+	cout << "\tpointer = " << &iNumber_2 << endl;
+
+	//call-by-reference
+	cout << "-----璸秨﹍-----\n";
+	iStartTime = clock();
+	add(iNumber_1, iNumber_2);
+	iEndTime = clock();
+	cout << "-----璸挡-----\n";
+
+	cout << "磅︽function把计\n";
+	cout << "把计1\n";
+	cout << "\tvalue = " << iNumber_1 << endl;
+	cout << "\treference = iNumber_1" << endl;
+	cout << "\tpointer = " << &iNumber_1 << endl;
+	cout << "把计2\n";
+	cout << "\tvalue = " << iNumber_2 << endl;
+	cout << "\treference = iNumber_2" << endl;
+	cout << "\tpointer = " << &iNumber_2 << endl;
+	cout << endl;
+
+	cout << "磅︽丁(ms) = " << iEndTime - iStartTime << endl;
+	cout << endl;
+
+	system("pause");
+	return 0;
+}
